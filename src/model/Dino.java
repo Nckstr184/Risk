23d6dc6ac397ac2c66cb6c1c4e4cb7d784b9c135
monicasproul <msproul@email.arizona.edu>
2015-11-08 22:05:59 -0700

    hi

--- src/model/Dino.java
+++ src/model/Dino.java
@@ -1,5 +1,28 @@
 package model;
 
+import java.awt.Point;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+
 public class Dino extends Continent {
+
+	ArrayList<Territory> dinoTerr;
+	public Dino(String cName, ArrayList<Territory> cTerritories,boolean cConquered) {
+		super(cName, cTerritories, cConquered);
+		
+		Player currentPlayer = null;//get current player
+		JButton button= new JButton("0");
+		// Sting name, Point tcoords, JButton tbutton, Player owner, int numOfUnits
+		dinoTerr.add(0, new Territory("Rawrville",new Point(880,280), button, currentPlayer, 0));
+		dinoTerr.add(1, new Territory("Laiegges",new Point(830,330), button, currentPlayer, 0));
+		dinoTerr.add(2, new Territory("Dactilito",new Point(830,410), button, currentPlayer, 0));
+		dinoTerr.add(3, new Territory("Dirtydan",new Point(770,410), button, currentPlayer, 0));
+		dinoTerr.add(4, new Territory("BlackBeard",new Point(790,470), button, currentPlayer, 0));
+		dinoTerr.add(5, new Territory("Monisaurus",new Point(730,520), button, currentPlayer, 0));
+		dinoTerr.add(6, new Territory("ToyStory",new Point(840,520), button, currentPlayer, 0));
+
+		
+	}
 
 }
--- src/model/Territory.java
+++ src/model/Territory.java
@@ -1,5 +1,49 @@
 package model;
 
+import java.awt.Point;
+
+import javax.swing.JButton;
+
 public class Territory {
+	
+	Point coords;
+	JButton button;
+	Player player;
+	int units;
+	double x;
+	double y;
+	String tname;
+	
+	public Territory(String name, Point tcoords, JButton tbutton, Player owner, int numOfUnits){
+		coords=tcoords;
+		button=tbutton;
+		player=owner;
+		units= numOfUnits;
+		tname=name;
+	}
+	public String getname(){
+		return tname;
+	}
+	public Point getPoint(){
+		return coords;
+	}
+	public double getX(){
+		x= coords.getX();
+		return x;
+	}
+	public double getY(){
+		y= coords.getY();
+		return y;
+	}
+	public JButton getButton(){
+		return button;
+	}
+	public Player getOwner(){
+		return player;
+	}
+	public int getUnits(){
+		return units;
+	}
+
 
 }
