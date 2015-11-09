e07e0a16b4da8099658f23f178557e48465d985f
zjhaddad <zjhaddad@email.arizona.edu>
2015-11-08 22:28:24 -0700

    Zach

--- src/model/Dice.java
+++ src/model/Dice.java
@@ -1,5 +1,67 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Dice {
+
+	private int diceRolled, numOfDice;
+	private double tempDice;
+	public ArrayList<Integer> diceList;
+	private Dice tester;
+	
+
+
+	public Dice(int numOfDiceRolled) {
+		this.numOfDice = numOfDiceRolled;
+		ArrayList<Integer> diceList = new ArrayList<Integer>();
+		if (numOfDice == 3) {
+			diceList.add(rollDice());
+			diceList.add(rollDice());
+			diceList.add(rollDice());
+			
+			
+		}
+		if (numOfDice == 2) {
+			diceList.add(rollDice());
+			diceList.add(rollDice());
+			
+		
+		}
+		if (numOfDice == 1) {
+			diceList.add(rollDice());
+			
+			
+		}
+		
+	
+		
+	}
+	public int rollDice(){
+		int max = 7;
+		int min = 1;
+		diceRolled = (int)(Math.random() * (max - min) + min);
+		
+		return diceRolled;
+	}
+	public ArrayList<Integer> getDice(){
+		return diceList;
+	}
+	
+	public int getDice1(){
+		return diceList.get(0);
+	}
+	public int getDice2(){
+		return diceList.get(2);
+	}
+	public int getDice3(){
+		return diceList.get(3);
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
