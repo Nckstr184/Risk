dbc584787b517569437226b1631ae16b518cc5b3
Nick Larson <nlarson@email.arizona.edu>
2015-11-10 10:47:00 -0700

    Various class updates and filling

--- src/model/GameLogic.java
+++ src/model/GameLogic.java
@@ -2,24 +2,28 @@ package model;
 
 import java.util.ArrayList;
 
-
 public class GameLogic {
 	private final int NUM_OF_TERRITORIES = 7;
-	
+
 	private ArrayList<Territory> allTerritories;
 	private ArrayList<Continent> allContinents;
-	private ArrayList<Player> allPlayers;
+	private PlayerCollection allPlayers;
+	private Dice attackDice, defendDice, turnDice;
 	private Languages languages;
 	private Dino dino;
 	private Wildcat wildcat;
 	private Pizza pizza;
 	private Crescent crescent;
 	private Sun sun;
-	
+
 	public GameLogic() {
 		allTerritories = new ArrayList<Territory>();
 		allContinents = new ArrayList<Continent>();
-		
+		allPlayers = new PlayerCollection();
+		attackDice = new Dice(3);
+		defendDice = new Dice(2);
+		turnDice = new Dice(1);
+
 		// Adding all the continent objects to an arraylist
 		allContinents.add(languages);
 		allContinents.add(dino);
@@ -27,18 +31,22 @@ public class GameLogic {
 		allContinents.add(pizza);
 		allContinents.add(crescent);
 		allContinents.add(sun);
-		
+
 		// Filling in the territories list
 		ArrayList<Territory> tempTerritories;
-		for(int i=0; i<allContinents.size(); i++) {
+		for (int i = 0; i < allContinents.size(); i++) {
 			tempTerritories = allContinents.get(i).getTerritories();
-			for(int j=0; j<NUM_OF_TERRITORIES; j++) {
+			for (int j = 0; j < NUM_OF_TERRITORIES; j++) {
 				allTerritories.add(tempTerritories.get(j));
 			}
 		}
 	}
-	
-	public void startGame(ArrayList<Player> allPlayers) {
-		this.allPlayers = allPlayers;
+	public void addPlayers(Player player) {
+		allPlayers.addPlayers(player);
 	}
+
+	public void startGame() {
+		
+	}
+
 }
--- src/model/PlayerCollection.java
+++ src/model/PlayerCollection.java
@@ -1,7 +1,25 @@
 package model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class PlayerCollection implements Serializable {
 
+	ArrayList<Player> playerList;
+
+	public PlayerCollection() {
+		playerList = new ArrayList<Player>();
+	}
+
+	public boolean addPlayers(Player player) {
+		if (playerList.size() < 6) {
+			playerList.add(player);
+			return true;
+		} else
+			return false;
+	}
+
+	public Player getPlayer(int index) {
+		return playerList.get(index);
+	}
 }
