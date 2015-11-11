6d1e3358e7f598388af7230c2a82f8b038db272c
zjhaddad <zjhaddad@email.arizona.edu>
2015-11-11 16:53:22 -0700

    Card Collection Test Update

--- src/tests/CardCollectionTest.java
+++ src/tests/CardCollectionTest.java
@@ -1,6 +1,7 @@
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.awt.Image;
 import java.util.ArrayList;
@@ -16,11 +17,73 @@ public class CardCollectionTest {
 	Image img1;
 	@Test
 	public void testGetTerritory() {
-		CardCollection collectionTest=new CardCollection();
+		
 		ArrayList<Card> cardList= new ArrayList<Card>();
 		RiskCard card1 = new RiskCard("d", "GARY", img1);
+		RiskCard card2 = new RiskCard("c", "SPONGEBOB", img1);		
 		assertEquals("d", card1.getTerritory());
 		assertNotEquals("f", card1.getTerritory());
+		assertEquals("c", card2.getTerritory());
+		assertNotEquals("d", card2.getTerritory());
+		
+	}
+	
+	@Test
+	public void testCollectin(){
+		CardCollection collectionTest=new CardCollection();
+		Card A = collectionTest.getCard(0);
+		Card B = collectionTest.getCard(1);
+		Card C = collectionTest.getCard(2);
+		String D = A.getType();
+		String E = B.getType();
+		String F = C.getType();
+		
+		assertEquals("SPONGEBOB", D);
+		assertNotEquals("PATRICK", D);
+		assertNotEquals("GARY", D);
+		
+		assertNotEquals("SPONGEBOB", E);
+		assertEquals("PATRICK", E);
+		assertNotEquals("GARY", E);
+		
+		assertNotEquals("SPONGEBOB", F);
+		assertNotEquals("PATRICK", F);
+		assertEquals("GARY", F);
+		
+	}
+	
+	@Test
+	public void testShuffle(){
+		CardCollection collectionTest1 = new CardCollection();
+		CardCollection collectionTest2 = new CardCollection();
+		
+		
+		
+		Card card1 = collectionTest1.getCard(1);
+		collectionTest1.shuffle();
+		Card shuffledCard1 = collectionTest1.getCard(1);
+		
+		assertNotEquals(card1, shuffledCard1);
+		
+		
+	}
+	@Test
+	public void testGetCard(){
+		CardCollection collectionTest1 = new CardCollection();
+		Card card1 = collectionTest1.getCard(1);
+		Card card2 = collectionTest1.getCard(2);
+	
+		
+		assertNotEquals(card1, card2);
+		assertEquals(card1,card1);
+		
+		Card card3 = collectionTest1.getCard(24);
+		Card card4 = collectionTest1.getCard(28);
+		
+		assertNotEquals(card1, card4);
+		assertNotEquals(card3,card4);
+		assertEquals(card4,card4);
+		
 	}
 	
 
