f07e2b08ffe6cb705d1bceacadb182b6136d8be9
esgar moreno <emoreno95@email.arizona.edu>
2015-11-10 18:23:15 -0700

    test inserting

--- src/tests/ContinentTest.java
+++ src/tests/ContinentTest.java
@@ -18,15 +18,15 @@ public class ContinentTest {
 	@Test
 	public void test() {
 		ArrayList<Territory> cTerritories=new ArrayList<Territory>();
-		Continent Crescent=new Crescent("Crescent", cTerritories, false);
+	//	Continent Crescent=new Crescent("Crescent", cTerritories, false);
 		Continent Dino=new Dino("Dino", cTerritories, false);
 		Continent Pizza=new Pizza("Pizza", cTerritories, false);
 		Continent Sun=new Sun("Sun", cTerritories, false);
 		
-		assertEquals("Crescent", Crescent.getName());
-		assertEquals("Crescent", Crescent.getName());
-		assertEquals("Crescent", Crescent.getName());
-		assertEquals("Crescent", Crescent.getName());
+		//assertEquals("Crescent", Crescent.getName());
+		assertEquals("Dino", Dino.getName());
+		assertEquals("Pizza", Pizza.getName());
+		assertEquals("Sun", Sun.getName());
 
 	}
 
