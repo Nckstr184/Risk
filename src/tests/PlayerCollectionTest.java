c44f5bbbf9c09127c0591f7c340984fa6b74fd74
esgar moreno <emoreno95@email.arizona.edu>
2015-11-10 20:50:10 -0700

    more tests

--- src/tests/PlayerCollectionTest.java
+++ src/tests/PlayerCollectionTest.java
@@ -2,13 +2,38 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import model.Human;
+import model.Player;
+import model.PlayerCollection;
 
 public class PlayerCollectionTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		PlayerCollection collectionTest=new PlayerCollection();
+		ArrayList<Player> playerList= new ArrayList<Player>();
+		Player newP=new Human("plaer", Color.RED, 0);
+		assertEquals(0, collectionTest.getNumOfPlayers());
+		assertEquals(true,collectionTest.addPlayers(newP));
+		assertEquals(1, collectionTest.getNumOfPlayers());
+		assertEquals(newP, collectionTest.getPlayer(0));
+		collectionTest.addPlayers(newP);
+		collectionTest.addPlayers(newP);
+		collectionTest.addPlayers(newP);
+		collectionTest.addPlayers(newP);
+		collectionTest.addPlayers(newP);
+		assertEquals(false,collectionTest.addPlayers(newP));
+		collectionTest.setPlayerList(playerList);
+		assertEquals(0, collectionTest.getNumOfPlayers());
+		assertEquals(true,collectionTest.addPlayers(newP));
+		assertEquals(1, collectionTest.getNumOfPlayers());
+		
+
 	}
 
 }
