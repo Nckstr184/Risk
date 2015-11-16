7db8b71c61c059a692f2fa0f3fce3566c50b711d
monicasproul <msproul@email.arizona.edu>
2015-11-16 13:24:54 -0700

    gui

--- src/view/GameBoard.java
+++ src/view/GameBoard.java
@@ -1,20 +1,23 @@
 package view;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import model.Continent;
 
-public class GameBoard {
+public class GameBoard extends JPanel{
 	
 	ArrayList<Continent> Continents;
-	ImageIcon map;
+	Image map;
+	JPanel board;
 	
-	
-	public GameBoard(){
-		
+		 
 		
 	}
 
-}
--- src/view/MainGUI.java
+++ src/view/MainGUI.java
@@ -1,21 +1,73 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.LayoutManager;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.RootPaneContainer;
+import javax.swing.WindowConstants;
 
-public class MainGUI extends JPanel {
+
+
+public class MainGUI extends JFrame {
 	
 	GameBoard map;
 	JTextField updateDisplay;
 	JButton toggleSound, quitGame, saveGame;
 	
 	
-	public MainGUI(){
+	
+	public static void main(String[] args) {
+		MainGUI window = new MainGUI();
+		window.setVisible(true);
+		
+	}
+
+	public MainGUI() {
+		this.setLayout(null);
+		Dimension sizeMe = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(sizeMe);
+		this.setLayout(null);
+		ImageIcon C1T1= new ImageIcon("./Pictures/circle.png");
+		JButton nbt= new JButton(C1T1);
+		
+//		GameBoard panel = new GameBoard((new ImageIcon("images/background.png").getImage()));
+//		panel.setLocation(300, 10);
+//	    panel.setSize(500,500);
+//	    this.add(panel);
+//	    panel.setVisible(true);
+//	    
+		
+		
+     // nbt.setBackground(Color.BLUE);
+      nbt.setSize(50, 50);
+      nbt.setLocation(100, 100);
+      nbt.setOpaque(false);
+      nbt.setContentAreaFilled(false);
+      nbt.setBorderPainted(false);
+      this.add(nbt);
+      nbt.setVisible(true);
+	        
+	    }
 		
 		
 		
 	}
 	
 
-}
+
