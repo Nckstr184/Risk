cc7f3366dde835ca1cad54225894cd94500139e5
Nick Larson <nlarson@email.arizona.edu>
2015-11-16 19:44:13 -0700

    Updated BattleLogic, should be done now

--- src/model/BattleLogic.java
+++ src/model/BattleLogic.java
@@ -36,7 +36,6 @@ public class BattleLogic {
 			if(attackDice.getDice().get(i) <= defendDice.getDice().get(i))
 				count++;
 		}
-		
 		return count;
 	}
 	
@@ -47,7 +46,6 @@ public class BattleLogic {
 			if(attackDice.getDice().get(i) > defendDice.getDice().get(i))
 				count++;
 		}
-		
 		return count;
 	}
 	
