4e1088807ffe70ef39d3c48ff3b9f0d54e1d6ce3
esgar moreno <emoreno95@email.arizona.edu>
2015-11-22 22:16:55 -0700

    still fixing battle logic

--- src/model/BattleLogic.java
+++ src/model/BattleLogic.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 //WE NEED TO CHECK TO SEE IF ATTACKER HAS MORE THAN ONE ARMY BEFORE WE RUN BATTLE LOGIC!!!!!!!!!!!////////
 
 public class BattleLogic {
-	private Player attacker, defender; 
+	private Player attacker, defender;
 	private Dice attackDice, defendDice;
 	private Territory attackingTerritory, defendingTerritory;
 	private int numOfAttackDice, numOfDefendDice, leastDiceOnField;
@@ -13,65 +13,59 @@ public class BattleLogic {
 	public BattleLogic(Player attacker, Player defender, Territory attackingTerritory, Territory defendingTerritory) {
 		this.attacker = attacker;
 		this.defender = defender;
-		this.attackingTerritory=attackingTerritory;
-		this.defendingTerritory=defendingTerritory;
+		this.attackingTerritory = attackingTerritory;
+		this.defendingTerritory = defendingTerritory;
 	}
 
 	public void attackPlayer(int numOfAttackDice, int numOfDefendDice) {
-	
+
 		this.numOfAttackDice = numOfAttackDice;
 		this.numOfDefendDice = numOfDefendDice;
-		
-		if(numOfAttackDice >= numOfDefendDice)
+
+		if (numOfAttackDice >= numOfDefendDice)
 			leastDiceOnField = numOfDefendDice;
 		else
 			leastDiceOnField = numOfAttackDice;
-		
+
 		// roll the attackers dice (1-3)
 		attackDice = new Dice(numOfAttackDice);
 
 		// roll the defending dice (1-2)
 		defendDice = new Dice(numOfDefendDice);
-		
+
 	}
-	
-	public int armiesAttackerLost() {
-		int count = 0;
-		
-		for(int i=0; i<leastDiceOnField;i++) {
-			if(attackDice.getDice().get(i) <= defendDice.getDice().get(i))
-				count++;
+
+	public void subtractArmies() {
+		int countAttack = 0;
+		int countDefend = 0;
+
+		for (int i = 0; i < leastDiceOnField; i++) {
+			if (attackDice.getDice().get(i) <= defendDice.getDice().get(i))
+				countAttack++;
+
+			if (attackDice.getDice().get(i) > defendDice.getDice().get(i)) {
+				countDefend++;
+			}
 		}
-		return count;
+		attackingTerritory.addUnits((-1) * countAttack);
+		defendingTerritory.addUnits((-1) * countDefend);
+
 	}
-	
-	public int armiesDefenderLost() {
-		int count = 0;
-		
-		for(int i=0; i<leastDiceOnField;i++) {
-			if(attackDice.getDice().get(i) > defendDice.getDice().get(i))
-				count++;
-		}
-		return count;
-	}
-	
-	public boolean attackerWin()
-	{
-		if(defendingTerritory.getUnits()<=0)
-		{
+
+	public boolean attackerWin() {
+		if (defendingTerritory.getUnits() <= 0) {
 			System.out.println("CHECK2");
 			return true;
 		}
 		return false;
 	}
-	
+
 	public boolean defenderWin() {
-		if(attackingTerritory.getUnits()<=0)
-		{
+		if (attackingTerritory.getUnits() <= 1) {
 			System.out.println("CHECK3");
 			return true;
 		}
 		return false;
 	}
-	
+
 }
